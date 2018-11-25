15c8a2d98d2d6fb59229e93ef0c0c2724ae39209
Fin Reinhard <fin.reinhard@icloud.com>
2018-11-25 21:55:43 +0100

    added some intents

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.intent('random', {}, function (request, response) {
         .shouldEndSession(true);
 });
 
+app.intent('AMAZON.HelpIntent', {}, function (request, response) {
+    response
+        .say('Du kannst mich nach einem Verb fragen oder die App wieder beenden.')
+        .reprompt('Was möchtest Du gerne tun?')
+        .shouldEndSession(false);
+});
+
+app.intent('AMAZON.StopIntent', {}, function (request, response) {
+    response.say('Halt Stopp!');
+});
+
+app.intent('AMAZON.CancelIntent', {}, function (request, response) {
+    response.say('Kein Problem. Deine Anfrage wurde gestoppt.');
+});
+
+app.post = function (request, response, type, exception) {
+    if (exception) {
+        return response.clear().say('Huch, da ist was schief gelaufen').send();
+    }
+};
+
 app.express({expressApp: express_app});
 
 express_app.listen(8080);
