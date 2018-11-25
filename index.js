8c605b18462546619ed81ea3650db517cf87977e
Fin Reinhard <fin.reinhard@icloud.com>
2018-11-25 21:05:00 +0100

    added skill function

---
+++ index.js
@@ -0,0 +1,18 @@
+var alexa = require('alexa-app');
+var express = require('express');
+var verbs = require('./verbs');
+
+var express_app = express();
+var app = new alexa.app('exotic-verbs');
+
+app.intent('random', {}, function (request, response) {
+    response
+        .say('Ganz exotisch finde ich ' + verbs[Math.floor(Math.random() * verbs.length)])
+        .shouldEndSession(true);
+});
+
+app.express({expressApp: express_app});
+
+express_app.listen(8001);
+
+module.exports = app;
